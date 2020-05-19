5532ec3c17849e0f0535a6c850247d7cd0535aa9
Benson Toh <benson@eatigo.com>
2020-05-19 18:13:38 +0800

    edit service worker

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -5,21 +5,8 @@ importScripts(
   "https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js"
 );
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCmJqdwkYlCsmEWhYj4gFLVEBZcwA0qz2U",
-  authDomain: "test-web-push-a8f76.firebaseapp.com",
-  databaseURL: "https://test-web-push-a8f76.firebaseio.com",
-  projectId: "test-web-push-a8f76",
-  storageBucket: "test-web-push-a8f76.appspot.com",
-  messagingSenderId: "295246109385",
-  appId: "1:295246109385:web:8b0b2a54ef81a00b694b18",
-  measurementId: "G-GGE0S7MWT9",
-};
-
 // eslint-disable-next-line
-const app = firebase.initializeApp(firebaseConfig);
-// eslint-disable-next-line
-const messaging = firebase.messaging(app);
+const messaging = firebase.messaging();
 
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus)
