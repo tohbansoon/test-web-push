cd7df1cd5da12787ad3a8ee4b68cc58d73b58a14
Benson Toh <benson@eatigo.com>
2020-05-19 15:59:38 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmJqdwkYlCsmEWhYj4gFLVEBZcwA0qz2U",
+  authDomain: "test-web-push-a8f76.firebaseapp.com",
+  databaseURL: "https://test-web-push-a8f76.firebaseio.com",
+  projectId: "test-web-push-a8f76",
+  storageBucket: "test-web-push-a8f76.appspot.com",
+  messagingSenderId: "295246109385",
+  appId: "1:295246109385:web:8b0b2a54ef81a00b694b18",
+  measurementId: "G-GGE0S7MWT9",
+};
+
+export const messagingPublickKey =
+  "BCIxRX1ICyX3qRj7_iZ_jDGKGGw2IRMZ819n1qkxoqDfFHKk5_voJAUdqwp4sGzg46H5WsbBULRXm0iiuVzWlsk";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { firebaseConfig, messagingPublickKey } from "./config";
+import firebase from "firebase";
+
+// step 1: register service worker
+registerFirebaseServiceWorker();
+
+// step 2: initialize app and firebase messaging
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(app);
+messaging.usePublicVapidKey(messagingPublickKey);
+
+// step 3: getFcmToken
+getFcmToken();
+
+// step 4: listen to fcmToken changed
+listenFcmTokenChanged();
+
+// step 5: start to listening notification message
+onListenWebPushNotif();
+
+function registerFirebaseServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./firebase-messaging-sw.js")
+      .then((registration) => {
+        console.log("Registration successful, scope is:", registration.scope);
+      })
+      .catch((err) => {
+        console.log("Service worker registration failed, error:", err);
+      });
+  }
+}
+
+function unRegisterFirebaseServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister();
+    });
+  }
+}
+
+function getFcmToken() {
+  messaging
+    .getToken()
+    .then((fcmToken) => {
+      if (fcmToken) {
+        // receoved fcmToken
+        console.log("fcmToken: ", fcmToken);
+      } else {
+        // Show permission request.
+        console.log(
+          "No Instance ID token available. Request permission to generate one."
+        );
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
+    });
+}
+
+function listenFcmTokenChanged() {
+  messaging.onTokenRefresh(() => {
+    console.log("fcm token changed...");
+    getFcmToken();
+  });
+}
+
+function onListenWebPushNotif() {
+  messaging.onMessage((payload) => {
+    console.log("Message received. ", payload);
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
